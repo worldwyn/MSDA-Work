578d5138fefcf7528685ebb5a0acc5e76d023114
Joseph Ellinghaus <InnoVition@Joseph-Ellinghaus-MacBook-Pro-i7.local>
2014-09-01 18:05:36 -0600

    IS 607 Week 1 Assignment

---
+++ Joseph Ellinghaus IS 607 Week 1.R
@@ -0,0 +1,4 @@
+##  Question 1:  R version 3.1.1 (2014-07-10) -- "Sock it to Me"
+##               RStudio Version 0.98.953
+##  Question 2: PostgreSQL 9.3
+##  Question 3: The data set "sales" contains 401,146 rows (observations) and 5 columns
